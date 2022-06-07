f14c43a31a99e18199e6806bbfa6da772cd0f82b
Will Feng (DPER) <willfeng@fb.com>
2022-06-07 11:23:18 -0700

    fix

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# The goal is to move the entire `torchoptim` out into a standalone repository / library
-# that people can pip install, since there is no Alpa / JAX related logic inside this folder.
-
-from .adam import adam
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     setup(
         name="torchoptim",
-        version=os.environ.get("VERSION"),
+        version='0.0.1',
         author="Alpa team",
         author_email="",
         description="torchoptim",
